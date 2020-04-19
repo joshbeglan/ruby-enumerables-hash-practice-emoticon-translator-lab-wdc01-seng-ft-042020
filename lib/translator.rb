0a13c2b14ca8feb1645d32f148909bce0ce09456
Josh Beglan <josh_beglan@yahoo.com>
2020-04-19 08:46:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,15 +13,13 @@ end
 
 def get_english_meaning(file_path, emoticon)
   library = load_library(file_path)
-  response = nil
+  
   library.each do |name, emoticons|
     if emoticons[:japanese] == emoticon
       return name
-    else
-      response = "Sorry, that emoticon was not found"
     end
   end
-  response
+  "Sorry, that emoticon was not found"
 end
 
 def get_japanese_emoticon(file_path, emoticon)
